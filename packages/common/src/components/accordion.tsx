adc4232f267613bac18f129f8a2579cabf98cae8
uanela <uanelaluiswayne@gmail.com>
2026-01-03 20:47:07 +0200

    chore(common): adding accordion

---
+++ packages/common/src/components/accordion.tsx
@@ -0,0 +1,108 @@
+import * as React from "react";
+import * as AccordionPrimitive from "@radix-ui/react-accordion";
+import { ChevronDownIcon } from "lucide-react";
+import { cn } from "../utils/shadcn-ui/utils";
+
+type ShadcnAcoordionProps = React.ComponentProps<
+  typeof AccordionPrimitive.Root
+>;
+
+export type AccordionItemTriggerProps = React.ComponentProps<
+  typeof AccordionPrimitive.Trigger
+>;
+export type AccordionItemContentProps = React.ComponentProps<
+  typeof AccordionPrimitive.Content
+>;
+export type AccordionItemProps = React.ComponentProps<
+  typeof AccordionPrimitive.Item
+>;
+
+export type AccordionProps = {
+  items: {
+    trigger: React.ReactNode;
+    triggerProps?: AccordionItemTriggerProps;
+    content: React.ReactNode;
+    contentProps: AccordionItemContentProps;
+    value?: string;
+    props?: AccordionItemProps;
+  }[];
+} & ShadcnAcoordionProps;
+
+function ShadcnAccordion({ ...props }: ShadcnAcoordionProps) {
+  return <AccordionPrimitive.Root data-slot="accordion" {...props} />;
+}
+
+function AccordionItem({ className, ...props }: AccordionItemProps) {
+  return (
+    <AccordionPrimitive.Item
+      data-slot="accordion-item"
+      className={cn("border-b last:border-b-0", className)}
+      {...props}
+    />
+  );
+}
+
+function AccordionTrigger({
+  className,
+  children,
+  ...props
+}: AccordionItemTriggerProps) {
+  return (
+    <AccordionPrimitive.Header className="flex">
+      <AccordionPrimitive.Trigger
+        data-slot="accordion-trigger"
+        className={cn(
+          "focus-visible:border-ring focus-visible:ring-ring/50 flex flex-1 items-start justify-between gap-4 rounded-md py-4 text-left text-sm font-medium transition-all outline-none hover:underline focus-visible:ring-[3px] disabled:pointer-events-none disabled:opacity-50 [&[data-state=open]>svg]:rotate-180",
+          className
+        )}
+        {...props}
+      >
+        {children}
+        <ChevronDownIcon className="text-muted-foreground pointer-events-none size-4 shrink-0 translate-y-0.5 transition-transform duration-200" />
+      </AccordionPrimitive.Trigger>
+    </AccordionPrimitive.Header>
+  );
+}
+
+function AccordionContent({
+  className,
+  children,
+  ...props
+}: React.ComponentProps<typeof AccordionPrimitive.Content>) {
+  return (
+    <AccordionPrimitive.Content
+      data-slot="accordion-content"
+      className="data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down overflow-hidden text-sm"
+      {...props}
+    >
+      <div className={cn("pt-0 pb-4", className)}>{children}</div>
+    </AccordionPrimitive.Content>
+  );
+}
+
+export default function Accordion({ items, ...props }: AccordionProps) {
+  return (
+    <ShadcnAccordion
+      type="single"
+      collapsible
+      className="w-full"
+      defaultValue={
+        props.type === "multiple"
+          ? [items?.[0]?.value || "item-1"]
+          : items?.[0]?.value || "item-1"
+      }
+      {...(props as any)}
+    >
+      {items.map((item, i) => (
+        <AccordionItem value={item.value || `item-${i + 1}`}>
+          <AccordionTrigger {...item.triggerProps}>
+            {item.trigger}
+          </AccordionTrigger>
+          <AccordionContent {...item.contentProps}>
+            {item.content}
+          </AccordionContent>
+        </AccordionItem>
+      ))}
+    </ShadcnAccordion>
+  );
+}
--- packages/common/src/exports/index.ts
+++ packages/common/src/exports/index.ts
@@ -1,5 +1,17 @@
 import HoverCard, { HoverCardProps } from "../components/hover-card";
 import Button from "../components/button";
+import Accordion, {
+  AccordionItemProps,
+  AccordionItemTriggerProps,
+  AccordionItemContentProps,
+  AccordionProps,
+} from "../components/accordion";
 
-export { HoverCard, Button };
-export type { HoverCardProps };
+export { HoverCard, Button, Accordion };
+export type {
+  HoverCardProps,
+  AccordionProps,
+  AccordionItemProps,
+  AccordionItemTriggerProps,
+  AccordionItemContentProps,
+};
