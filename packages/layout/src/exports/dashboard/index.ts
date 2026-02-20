e073bb2719e5d632636faec04c78c555baf0dee9
uanela <uanelaluiswayne@gmail.com>
2026-02-21 01:14:37 +0200

    chore(layout): export component for client usage

---
+++ packages/layout/src/exports/dashboard/index.ts
@@ -0,0 +1,4 @@
+import DashboardSidebar from "../../components/dashboard/sidebar/sidebar";
+import DashboardMainLayout from "../../components/dashboard/main-layout";
+
+export { DashboardSidebar, DashboardMainLayout };
