50ed9a836a80568f772fe4fb4cccf75289c46e49
uanela <uanelaluiswayne@gmail.com>
2026-04-19 13:20:16 +0200

    feat(core): export Link component from package entry

--- packages/core/src/exports/index.ts
+++ packages/core/src/exports/index.ts
@@ -1,6 +1,7 @@
 import Provider, { ProviderConfig } from "../components/provider";
+import Link from "../components/link";
 import { useProvider } from "../hooks/use-provider";
 import { RouterAdapter } from "../types";
 
-export { Provider, useProvider };
+export { Provider, useProvider, Link };
 export type { ProviderConfig, RouterAdapter };
